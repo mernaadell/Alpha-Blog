8cb703b5b884e308e392015d28a403597331cbfe
merna <mernaadel963@gmail.com>
2021-05-27 23:31:48 +0200

    create table model and add validation

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,25 @@
+class ArticlesController < ApplicationController
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def store
+
+  end
+
+  def destroy
+
+  end
+end
